4ab8447f44e666aeb12f626e29a3e3842785eb57
vknguyen1992 <nguyen.vu@trustingsocial.com>
2020-06-08 23:19:06 +0700

    Create Home and Login pages.

---
+++ example/iris-data-classifier/ML-React-App-Template/ui/src/Screens/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+          Home Page
+      </div>
+    );
+  }
+}
+
+export default Home;
