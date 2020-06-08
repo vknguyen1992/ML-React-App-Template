6024450b6fa10761b068085acab8635a5aeaf1a8
vknguyen1992 <nguyen.vu@trustingsocial.com>
2020-06-08 23:19:06 +0700

    Create Home and Login pages.

---
+++ example/iris-data-classifier/ML-React-App-Template/ui/src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Screens/Home';
+import Login from './Screens/Login';
+
+const Main = () => {
+  return (
+    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/login' component={Login}></Route>
+    </Switch>
+  );
+}
+
+export default Main;
