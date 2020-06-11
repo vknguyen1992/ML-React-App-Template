1d26e5ef71ec938ef39d2bc8f99987b579d93c5c
vknguyen1992 <nguyen.vu@trustingsocial.com>
2020-06-11 18:37:03 +0700

    Add mri viewer.

---
+++ example/iris-data-classifier/ML-React-App-Template/ui/src/Screens/MriViewer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MriViewer extends Component {
+  render() {
+    return (
+      <div className="MriViewer">
+          MRI Viewer
+      </div>
+    );
+  }
+}
+
+export default MriViewer;
